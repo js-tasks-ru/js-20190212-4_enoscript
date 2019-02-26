f051f51616de197e849f2d1acb8ee6162d582689
raccoonAndy <byhachi11@gmail.com>
2019-02-26 23:19:35 +0300

    решение 3-module/1-task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,6 +5,13 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+    let result = '';
+    const persons = data.filter(person => person['age'] <= age);
+
+    for (let i = 0; i < persons.length; i++) {
+        let str = `${persons[i]['name']}, ${persons[i]['balance']}`;
+        result += result ? `\n${str}` : `${str}`;
+    }
+
+    return result;
 }
-
-
