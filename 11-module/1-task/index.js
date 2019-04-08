253aa6090ecc3a14d04a152a25c64621e0a8e6d5
k.deniakova <byhachi11@gmail.com>
2019-04-08 15:21:08 +0300

    решение 11-module/1-task

--- 11-module/1-task/index.js
+++ 11-module/1-task/index.js
@@ -28,6 +28,43 @@
 
             this.el.classList.add(this.name);
             document.body.appendChild(this.el);
+
+            this.addTooltipBind = this.addTooltip.bind(this);
+            this.removeTooltipBind = this.removeTooltip.bind(this);
+        }
+
+        addTooltip(event) {
+            let tooltip = this.el;
+            if (!event.target.matches('span')) return;
+
+            if (!tooltip) {
+                return;
+            }
+
+            tooltip.innerHTML = event.target.dataset.tooltip;
+            tooltip.classList.add(this.name +'_active');
+
+            const coords = event.target.getBoundingClientRect();
+
+            let left = coords.left;
+            if (left < 0) {
+                left = 0;
+            }
+
+            let top = coords.top - tooltip.offsetHeight - this.indent;
+            if (top < 0) {
+                top = coords.top + tooltip.offsetHeight + this.indent;
+            }
+
+            tooltip.style.left = `${left}px`;
+            tooltip.style.top = `${top}px`;
+        }
+
+        removeTooltip() {
+            let tooltip = this.el;
+
+            tooltip.innerHTML = '';
+            tooltip.classList.remove(this.name +'_active');
         }
 
         /**
@@ -37,12 +74,24 @@
          * @param {Element} root - элемент, внутри которого, нужно включить работу подсказок
          */
         attach(root) {
+            if (!root) return;
+            const spans = root.querySelectorAll('[data-tooltip]');
+            spans.forEach(span => {
+                span.addEventListener('mouseover', this.addTooltipBind);
+                span.addEventListener('mouseout', this.removeTooltipBind);
+            })
+
         }
 
         /**
          * Удаляет все обработчики событий
          */
         detach() {
+            const spans = document.querySelectorAll('[data-tooltip]');
+            spans.forEach(span => {
+                span.removeEventListener('mouseover', this.addTooltipBind);
+                span.removeEventListener('mouseout', this.removeTooltipBind);
+            })
         }
     }
 
